9e1ef7e8691b8a8a780313c9dbc706f949b08fd2
Angela <160551855+AngelaNova@users.noreply.github.com>
2024-03-07 15:31:32 -0500

    Update DayOfTheWeek.java

---
+++ DayOfTheWeek.java
@@ -0,0 +1,48 @@
+public class DayOfTheWeek {
+
+ public static void main(String[] args) {
+ 
+  /*
+  Angela Novakovic 260727217
+  */
+ 
+  //Declaring the variables for year(y), month(m), day(d)
+  int y,m,d;
+  //Initialisation of the variables y,m and d with the input arguments 
+  y = Integer.valueOf(args[0]);
+  m = Integer.valueOf(args[1]);
+  d = Integer.valueOf(args[2]);
+  //Declaring the auxiliary variables used in the formula.
+  int y0, m0, d0, x;
+  
+  
+  //Your code Starts here
+  //Writing operation to calculate the day of the week. Giving values to the variables y0, x, m0 and d0.
+      y0= ( y -(14-m)/12);
+      x= (y0 + y0/4 - y0/100 + y0/400);
+      m0= (m + (12*((14-m)/12))-2);
+      d0= (( d + x + 31*m0/12)%7);
+      
+      //Writing conditional statements in a chain
+      if ( d0==0) {System.out.println ( "Sunday" );}
+      else if( d0==1) 
+        { System.out.println ("Monday");}
+      else if (d0==2)
+        {System.out.println ("Tuesday");}
+      else if (d0==3)
+        {System.out.println ("Wednesday");}
+      else if (d0==4)
+        {System.out.println ("Thursday");}
+      else if (d0==5)
+        {System.out.println ("Friday");}
+      else {System.out.println ("Sunday");}
+      
+  
+       
+   
+  
+  
+  //Your code Ends here
+ }
+
+}

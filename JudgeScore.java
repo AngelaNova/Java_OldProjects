e2c6d6304e15502cadc46dafac97c5e1af790cb6
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-03-13 14:01:28 -0400

    Update JudgeScore.java

--- JudgeScore.java
+++ JudgeScore.java
@@ -1,48 +1,55 @@
-public class DayOfTheWeek {
-
+public class JudgeScore {
  public static void main(String[] args) {
+
  
-  /*
-  Angela Novakovic 260727217
-  */
+  //Declaring the variables for storing the judges scores.
+  int judge1, judge2, judge3, judge4;
+  judge1 = Integer.valueOf(args[0]);
+  judge2 = Integer.valueOf(args[1]);
+  judge3 = Integer.valueOf(args[2]);
+  judge4 = Integer.valueOf(args[3]);
+  
+
+  
+  //Declaring the variable max1, max2 and max for storing the highest score 
+  //max1 is the highest score between judge1's and judge2's scores
+  //max2 is the highest score between max1's and judge3's scores
+  //max is the highest score that was received from the judges
+  int max1,max2,max;
+  
+  //Finding the maximum value by using conditional statements (if,else)in a chain form
+  if (judge1>judge2) { max1=judge1;}
+  else { max1=judge2;}
+  if (judge3>max1) {max2=judge3;}
+  else {max2=max1;}
+  if (judge4>max2) {max=judge4;}
+  else {max=max2;}
+  
  
-  //Declaring the variables for year(y), month(m), day(d)
-  int y,m,d;
-  //Initialisation of the variables y,m and d with the input arguments 
-  y = Integer.valueOf(args[0]);
-  m = Integer.valueOf(args[1]);
-  d = Integer.valueOf(args[2]);
-  //Declaring the auxiliary variables used in the formula.
-  int y0, m0, d0, x;
+  //Declaring the variable min1, min2 and min for storing the lowest score 
+  //min1 is the lowest score between judge1's and judge2's scores
+  //min2 is the lowest score between min1's and judge3's scores
+  //min is the lowest score that was received from the judges
+  int min1,min2,min;
+  
+  //Finding the minimum value by using conditional statements (if,else)in a chain form
+  if (judge1<judge2){ min1=judge1;}
+  else { min1=judge2;}
+  if (judge3<min1) {min2=judge3;}
+  else {min2=min1;}
+  if (judge4<min2) {min=judge4;}
+  else {min=min2;}
   
   
-  //Your code Starts here
-  //Writing operation to calculate the day of the week. Giving values to the variables y0, x, m0 and d0.
-      y0= ( y -(14-m)/12);
-      x= (y0 + y0/4 - y0/100 + y0/400);
-      m0= (m + (12*((14-m)/12))-2);
-      d0= (( d + x + 31*m0/12)%7);
-      
-      //Writing conditional statements in a chain
-      if ( d0==0) {System.out.println ( "Sunday" );}
-      else if( d0==1) 
-        { System.out.println ("Monday");}
-      else if (d0==2)
-        {System.out.println ("Tuesday");}
-      else if (d0==3)
-        {System.out.println ("Wednesday");}
-      else if (d0==4)
-        {System.out.println ("Thursday");}
-      else if (d0==5)
-        {System.out.println ("Friday");}
-      else {System.out.println ("Sunday");}
-      
+  //We are doing a typecasting from integer to double to provide a more precise answer.
+  double average= (double)( (judge1+judge2+judge3+judge4-max-min)/2.0);
   
-       
+    //  System.out.print("The average score is: ");
+  //This statement shows on the screen what value of type double was stored in the variable "average".
+      System.out.println (average);
    
   
   
-  //Your code Ends here
+  
  }
-
 }

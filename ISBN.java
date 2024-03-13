ede8df0a29d059ec7e66c5b2df275a0003935aa4
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-03-13 13:56:41 -0400

    Update ISBN.java

--- ISBN.java
+++ ISBN.java
@@ -1,48 +1,51 @@
-public class DayOfTheWeek {
+public class ISBN {
 
  public static void main(String[] args) {
  
-  /*
-  Angela Novakovic 260727217
-  */
  
-  //Declaring the variables for year(y), month(m), day(d)
-  int y,m,d;
-  //Initialisation of the variables y,m and d with the input arguments 
-  y = Integer.valueOf(args[0]);
-  m = Integer.valueOf(args[1]);
-  d = Integer.valueOf(args[2]);
-  //Declaring the auxiliary variables used in the formula.
-  int y0, m0, d0, x;
+  //Declaring the variable to represent the ISBN number
+        int n = Integer.parseInt(args[0]);
+        
+    
+    /*declaring variables that will represent the digits in the position 5, 4, 3 and 2 
+     * + the equation with which it is possible to determine the value of the digit in position 1
+    */
+    int d5,d4,d3,d2,d1,equation;
+    
+    
+    /*Using operations to obtain the digit for each position and to obtain an equation to find 
+     * the value of the digit number 1 that . 
+    *Assigning values to the variables d5, d4, d3, d2 and equation
+    */
+    
+    d5=n/1000;
+    d4=(n/100)%10;
+    d3=(n/10)%10;
+    d2=n%10;
+    
+    /*Using a equation where the sum of all the digit multiplied by the number of the position in which they are
+     * is a multiple of 11, if not d1 will be the smallest number that must be able for the equation to be
+     * a multiple of 11
+    */
+    
+    equation=(5*d5+4*d4+3*d3+2*d2)%11;
+
   
-  
-  //Your code Starts here
-  //Writing operation to calculate the day of the week. Giving values to the variables y0, x, m0 and d0.
-      y0= ( y -(14-m)/12);
-      x= (y0 + y0/4 - y0/100 + y0/400);
-      m0= (m + (12*((14-m)/12))-2);
-      d0= (( d + x + 31*m0/12)%7);
-      
-      //Writing conditional statements in a chain
-      if ( d0==0) {System.out.println ( "Sunday" );}
-      else if( d0==1) 
-        { System.out.println ("Monday");}
-      else if (d0==2)
-        {System.out.println ("Tuesday");}
-      else if (d0==3)
-        {System.out.println ("Wednesday");}
-      else if (d0==4)
-        {System.out.println ("Thursday");}
-      else if (d0==5)
-        {System.out.println ("Friday");}
-      else {System.out.println ("Sunday");}
-      
-  
-       
-   
-  
-  
-  //Your code Ends here
+    //Using conditional statement in chain form to obtain the first digit  
+    if (equation==0) {System.out.println(0);}
+    else if (equation==1) {System.out.println("X");}
+    else if (equation==2) {System.out.println(9);}
+    else if (equation==3) {System.out.println(8);}
+    else if (equation==4) {System.out.println(7);}
+    else if (equation==5) {System.out.println(6);}
+    else if (equation==6) {System.out.println(5);}
+    else if (equation==7) {System.out.println(4);}
+    else if (equation==8) {System.out.println(3);}
+    else if (equation==9) {System.out.println(2);}
+    else if (equation==10) {System.out.println(1);}
+    
+        
+        
  }
 
 }
